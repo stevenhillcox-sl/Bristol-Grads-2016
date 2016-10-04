bac43a5b9552ab56b51cb46f9774b90a9dfff769
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-10-04 18:20:25 +0100

    admin now sees what the client sees

--- client/angular/controllers/AdminController.js
+++ client/angular/controllers/AdminController.js
@@ -22,6 +22,12 @@
         $scope.errorMessage = "";
         $scope.blockedUsers = [];
 
+        $scope.visitorsTweets = [];
+        $scope.speakersTweets = [];
+        $scope.pinnedTweets = [];
+        $scope.extraPinnedTweets = [];
+        $scope.extraSpeakersTweets = [];
+
         $scope.setDeletedStatus = adminDashDataService.setDeletedStatus;
         $scope.setPinnedStatus = adminDashDataService.setPinnedStatus;
         $scope.addSpeaker = addSpeaker;
@@ -129,6 +135,58 @@
             }
         }
 
+        function splitTweetsIntoCategories(tweets) {
+            $scope.visitorsTweets = [];
+            $scope.speakersTweets = [];
+            $scope.pinnedTweets = [];
+            $scope.extraPinnedTweets = [];
+            $scope.extraSpeakersTweets = [];
+            var pinnedCount = 0;
+            var visitorsCount = 0;
+            var speakersCount = 0;
+            var tweetCount;
+            var i;
+            for (i = 0; i < tweets.length; i++) {
+                tweetCount = tweets[i].entities.media !== undefined ? 2 : 1;
+                if (!(tweets[i].deleted || tweets[i].blocked) || tweets[i].display) {
+                    if (tweets[i].pinned) {
+                        if (pinnedCount + tweetCount < 5) {
+                            $scope.pinnedTweets.push(tweets[i]);
+                            pinnedCount += tweetCount;
+                        }
+                    } else if (tweets[i].wallPriority) {
+                        if (speakersCount + tweetCount < 6) {
+                            $scope.speakersTweets.push(tweets[i]);
+                            speakersCount += tweetCount;
+                        }
+                    } else {
+                        if (visitorsCount + tweetCount < 6) {
+                            $scope.visitorsTweets.push(tweets[i]);
+                            visitorsCount += tweetCount;
+                        } else {
+                            if (speakersCount + tweetCount < 6) {
+                                $scope.extraSpeakersTweets.push(tweets[i]);
+                                speakersCount += tweetCount;
+                            } else if (pinnedCount + tweetCount < 5) {
+                                $scope.extraPinnedTweets.push(tweets[i]);
+                                pinnedCount += tweetCount;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        function compare(a, b) {
+            if (new Date(a.created_at) > new Date(b.created_at)) {
+                return -1;
+            }
+            if (new Date(a.created_at) < new Date(b.created_at)) {
+                return 1;
+            }
+            return 0;
+        }
+
         $scope.setFlagsForTweets = function(tweets, updates) {
             updates.forEach(function(update) {
                 if (update.type === "tweet_status") {
@@ -140,15 +198,29 @@
                             updatedTweet[prop] = update.status[prop];
                         }
                     }
-                }
-                if (update.type === "user_block") {
+                } else if (update.type === "user_block") {
                     tweets.forEach(function(tweet) {
                         if (tweet.user.screen_name === update.screen_name) {
                             tweet.blocked = update.blocked;
                         }
                     });
+                } else if (update.type === "speaker_update") {
+                    var wallPriority;
+                    if (update.operation === "add") {
+                        wallPriority = true;
+                    } else if (update.operation === "remove") {
+                        wallPriority = false;
+                    }
+                    tweets.forEach(function(tweet) {
+                        if (tweet.user.screen_name === update.screen_name) {
+                            tweet.wallPriority = wallPriority;
+                        }
+                        return tweet;
+                    });
                 }
             });
+            tweets = tweets.sort(compare);
+            splitTweetsIntoCategories(tweets);
             return tweets;
         };
     }
