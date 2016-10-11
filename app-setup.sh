7af23183b339212bcf797990493f6148d0a8613c
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-10-11 17:53:29 +0100

    changed

--- app-setup.sh
+++ app-setup.sh
@@ -1,1 +1,11 @@
-npm install
+# update instance
+yum -y update
+
+echo "Installing nodejs to yum"
+curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
+yum -y install nodejs
+
+echo "Installing packages"
+sudo npm install
+
+echo "Installed required packages"
--- app-start.sh
+++ app-start.sh
@@ -1,1 +1,3 @@
+echo "Starting application"
 node /server.js
+echo "Application started successfully !"
