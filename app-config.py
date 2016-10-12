d33b27efb0c91404e6ae447934773862d46b55a6
Georgiana Ginghina <gginghina@scottlogic.co.uk>
2016-10-12 10:32:22 +0100

    changed python reference

--- app-config.py
+++ app-config.py
@@ -1,60 +1,37 @@
 #!/usr/bin/python
 
-import os
+import json
+from pprint import pprint
+
+with open('/home/ec2-user/secret.json') as data_file:    
+    data = json.load(data_file)
+
+pprint(data)
+ 
+fp=open("/server.js","r")
+buffer=fp.read()
+fp.close()
  
 strToSearch="process.env.TWEET_WALL_OAUTH_SECRET"
-strToReplace=os.environ['TWEET_WALL_OAUTH_SECRET']
-
-fp=open("/server.js","r")
-buffer=fp.read()
-fp.close()
- 
-fp=open("/server.js","w")
-fp.write(buffer.replace(strToSearch,strToReplace))
-fp.close()
+strToReplace=data['TWEET_WALL_OAUTH_SECRET']
+buffer = buffer.replace(strToSearch,strToReplace)
 
 strToSearch="process.env.TWITTER_CONSUMER_KEY"
-strToReplace=os.environ['TWITTER_CONSUMER_KEY']
- 
-fp=open("/server.js","r")
-buffer=fp.read()
-fp.close()
- 
-fp=open("/server.js","w")
-fp.write(buffer.replace(strToSearch,strToReplace))
-fp.close()
+strToReplace=data['TWITTER_CONSUMER_KEY']
+buffer = buffer.replace(strToSearch,strToReplace)
 
 strToSearch="process.env.TWITTER_CONSUMER_SECRET"
-strToReplace=os.environ['TWITTER_CONSUMER_SECRET']
- 
-fp=open("/server.js","r")
-buffer=fp.read()
-fp.close()
- 
-fp=open("/server.js","w")
-fp.write(buffer.replace(strToSearch,strToReplace))
-fp.close()
-
+strToReplace=data['TWITTER_CONSUMER_SECRET']
+buffer = buffer.replace(strToSearch,strToReplace)
 
 strToSearch="process.env.TWITTER_ACCESS_TOKEN_KEY"
-strToReplace=os.environ['TWITTER_ACCESS_TOKEN_KEY']
- 
-fp=open("/server.js","r")
-buffer=fp.read()
-fp.close()
- 
-fp=open("/server.js","w")
-fp.write(buffer.replace(strToSearch,strToReplace))
-fp.close()
+strToReplace=data['TWITTER_ACCESS_TOKEN_KEY']
+buffer = buffer.replace(strToSearch,strToReplace)
 
 strToSearch="process.env.TWITTER_ACCESS_TOKEN_SECRET"
-strToReplace=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
- 
-fp=open("/server.js","r")
-buffer=fp.read()
-fp.close()
- 
-fp=open("/server.js","w")
-fp.write(buffer.replace(strToSearch,strToReplace))
-fp.close()
+strToReplace=data['TWITTER_ACCESS_TOKEN_SECRET']
+buffer = buffer.replace(strToSearch,strToReplace)
 
+fp=open("/server.js","w")
+fp.write(buffer)
+fp.close()
